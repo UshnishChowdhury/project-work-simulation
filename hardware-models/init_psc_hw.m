cec5dff09706459c990f6e23ea46f8676eb77406
maattaj11 <juho.k.maatta@aalto.fi>
2024-05-06 15:10:22 +0300

    increased number of signals read from board to 8

--- hardware-models/init_psc_hw.m
+++ hardware-models/init_psc_hw.m
@@ -9,7 +9,7 @@ L_f = 1e-3;            % Inductance of the filter inductor
 L_f1 = L_f;
 L_f2 = L_f;
 C = 1e-3;               % DC-bus capacitance
-w_g = 2*pi*50;          % Grid angular frequency
+w_N = 2*pi*50;          % Grid angular frequency
 u_gN = 24;              % Nominal grid voltage (phase-to-neutral, peak value)
 w0_pll = 2*pi*20;
 R_L = 12;               % Load resistance
@@ -27,16 +27,14 @@ div = 1;                % system clock divider
 carr = 2;               % carrier style, 2 for up-down
 T_pwm = f_clk/(div*f_sw*carr); % PWM timer period
 
-% Reference parameters
-E_p = 24.0;             % Reference grid voltage
-
 % PSC/RFPSC controller parameters
-w_b = 0.1*w_g;          % Filter bandwidth
+w_b = 0.1*w_N;          % Filter bandwidth
 R_a = 0.32;             % Active resistance
 K = 1;                  % Space vector scaling constant
 kpp = 3/2 * K^(-2);     % Peak value vector scaling
-K_p = 3*w_g*R_a/(2*u_gN^2*kpp); % Active power control gain
+K_p = 3*w_N*R_a/(2*u_gN^2*kpp); % Active power control gain
 
-I_max = 5;              % Current limit for software overcurrent protection
+I_max = 7;              % Current limit for software overcurrent protection
+U_dcmin = 35;           % Minimum DC voltage
 
-n=600;                  % Number of data points (samples) in one packet
+n=300;                  % Number of data points (samples) in one packet
